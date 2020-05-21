53c2fd1eec954c9c7f54613485f8c2487b0081b7
Paschal Ihenacho <ihenachopaschal@yahoo.com>
2020-05-21 01:54:50 -0500

    Bar graph showing all US States and case stats

--- script.js
+++ script.js
@@ -9,106 +9,110 @@
 // //     }
 // var baseURL =  + countryName;
 //var countryName  = '';
-$(document).ready(function(){
+
 // var chartURL = "https://thevirustracker.com/timeline/map-data.json"
 
 // var chartURL = "https://api.apify.com/v2/datasets/jaycEQiGMlb7aOmtI/items?format=json&clean=1"
-var chartURL = "https://api.covid19india.org/data.json"
+//var chartURL = "https://api.covid19india.org/data.json"
 
- $.ajax({
-     url: chartURL,
-    method: "GET",
+// var stateURL = "https://covidtracking.com/api/states";
+// var usURL = "http://covidtracking.com/api/us";
+// var infoURL = "https://covidtracking.com/api/states/info";
 
- }).then(function(data) {
+//  $.ajax({
+//      url: stateURL,
+//     method: "GET",
+
+//  }).then(function(data) {
   
-    console.log(data)
+//     console.log(data + "USA")
    
-  var total_active, total_recovered, total_deaths, total_confirmed;
+//   var total_active, total_recovered, total_deaths, total_confirmed;
 
-  // 4 empty arrays for chart
-   var state = []
-   var confirmed = []
-   var recovered = []
-   var deaths = []
+//   // 4 empty arrays for chart
+//    var state = []
+//    var confirmed = []
+//    var recovered = []
+//    var deaths = []
 
-    $.each(data.statewise, function(id, obj){
-        state.push(obj.state)
-        confirmed.push(obj.confirmed)
-        recovered.push(obj.recovered)
-        deaths.push(obj.deaths)
+//     $.each(data.statewise, function(id, obj){
+//         state.push(obj.state)
+//         confirmed.push(obj.confirmed)
+//         recovered.push(obj.recovered)
+//         deaths.push(obj.deaths)
 
   
-  })
+//   })
 
-    state.shift()
-    confirmed.shift()
-    recovered.shift()
-    deaths.shift()
+//     state.shift()
+//     confirmed.shift()
+//     recovered.shift()
+//     deaths.shift()
 
-   // console.log(state)
+//    // console.log(state)
      
 
-   total_active = data.statewise[0].active;
-   total_confirmed = data.statewise[0].confirmed;
-   total_recovered = data.statewise[0].recovered;
-   total_deaths = data.statewise[0].deaths;
+//    total_active = data.statewise[0].active;
+//    total_confirmed = data.statewise[0].confirmed;
+//    total_recovered = data.statewise[0].recovered;
+//    total_deaths = data.statewise[0].deaths;
 
-   $("#active").append(total_active);
-   $("#confirmed").append(total_confirmed);
-   $("recovered").append(total_recovered);
-   $("#deceased").append(total_deaths);
+//    $("#active").append(total_active);
+//    $("#confirmed").append(total_confirmed);
+//    $("recovered").append(total_recovered);
+//    $("#deceased").append(total_deaths);
    
    
-var myChart = document.getElementById('myChart').getContext('2d');
+// var myChart = document.getElementById('myChart').getContext('2d');
 
-var chart = new Chart(myChart, {
-    type: 'bar',
-    fill: false,
-    data: {
-        labels: state,
-        datasets: [
-            {
-                label: "Confirmed Cases",
-                data: confirmed,
-                backgroundColor: "#f1c40f",
-                minBarLength: 100
-            },
-            {
-                label: "Recovered Cases",
-                data: recovered,
-                backgroundColor: "green",
-                minBarLength: 100
-            }
-        ]
-    },
-    options:{}
-})
-
-})
+// var chart = new Chart(myChart, {
+//     type: 'bar',
+//     fill: false,
+//     data: {
+//         labels: state,
+//         datasets: [
+//             {
+//                 label: "Confirmed Cases",
+//                 data: confirmed,
+//                 backgroundColor: "#f1c40f",
+//                 minBarLength: 100
+//             },
+//             {
+//                 label: "Recovered Cases",
+//                 data: recovered,
+//                 backgroundColor: "green",
+//                 minBarLength: 100
+//             }
+//         ]
+//     },
+//     options:{}
+// })
 
 
-  var settings = {
-    "async": true,
-    "crossDomain": true,
-    "url": "https://covid-19-data.p.rapidapi.com/report/country/name?date-format=YYYY-MM-DD&format=json&date=2020-05-01&name=usa",
-    "method": "GET",
-    "headers": {
-        "x-rapidapi-host": "covid-19-data.p.rapidapi.com",
-        "x-rapidapi-key": "409edfd192msh76f5ae7cd381374p17ad34jsn668e30d41ee5"
-    }
-}
+
+
+//   var settings = {
+//     "async": true,
+//     "crossDomain": true,
+//     "url": "https://covid-19-data.p.rapidapi.com/report/country/name?date-format=YYYY-MM-DD&format=json&date=2020-05-01&name=usa",
+//     "method": "GET",
+//     "headers": {
+//         "x-rapidapi-host": "covid-19-data.p.rapidapi.com",
+//         "x-rapidapi-key": "409edfd192msh76f5ae7cd381374p17ad34jsn668e30d41ee5"
+//     }
+// }
 //var  queryParams = $("#countryInput").val();
-$.ajax(settings).done(function (response) {
-    console.log(response[0].provinces[0]);
-});
+// $.ajax(settings).done(function (response) {
+//     console.log(response[0].provinces[0]);
+// });
 
 
- })
+//  })
 
 
 
 function buildQueryURL() {
-    if(queryParams != '') {
+   // if(queryParams != '') {
     // queryURL is the url we'll use to query the API
         
    // ******IMPORTANT******
@@ -125,9 +129,9 @@ function buildQueryURL() {
     console.log(queryParams)
     return queryURL + queryParams;
     
-}else{
-              $("#error").html('Field cannot be empty');
-        }
+// }else{
+//               $("#error").html('Field cannot be empty');
+//         }
   }
 
 
@@ -271,6 +275,8 @@ $.ajax({
         $('#global-deaths').text (totalDeathsGlobal)
      })
 
+     $(document).ready(function(){
+
   //  var TestqueryURL =  "https://api.covid19api.com/live/country/China"
     //https://api.covid19api.com/summary
     var TestqueryURL = "https://api.covid19api.com/live/country/US/status/confirmed"
@@ -280,13 +286,103 @@ $.ajax({
     method: "GET",
 
 }).then(function(data) {
-  // console.log(data)
-   console.log(data)
-   var totalDeath = 0;
-   for (i = 0; i < data.length; i++) {
-       totalDeath += data[i].Deaths;
-   }
-   console.log(totalDeath);
+  console.log(data)
+//    var totalDeath = 0;
+//    for (i = 0; i < data.length; i++) {
+//        totalDeath += data[i].Deaths;
+//    }
+//    console.log(totalDeath);
 
 
+})
+
+var infoURL = "https://covidtracking.com/api/states/info";
+var stateURL = "https://covidtracking.com/api/states";
+
+$.ajax({
+ url: infoURL,
+ method: "GET",
+
+}).then(function(info) {
+console.log(info)
+
+
+})
+
+$.ajax({
+    url: stateURL,
+    method: "GET",
+   
+   }).then(function(stateData) {
+   console.log(stateData)
+   var total_recovered, total_deaths, total_confirmed;
+
+   // 4 empty arrays for chart
+    var state = []
+    var positive = []
+    var recovered = []
+    var death = []
+ 
+     $.each(stateData, function(id, obj){
+         state.push(obj.state)
+         positive.push(obj.positive)
+         recovered.push(obj.recovered)
+         death.push(obj.death)
+ 
+   
+   })
+ 
+    //  state.shift()
+    //  confirmed.shift()
+    //  recovered.shift()
+    //  deaths.shift()
+ 
+    // console.log(state)
+      
+    total_confirmed = stateData[0].positive;
+    total_recovered = stateData[0].recovered;
+    total_deaths = stateData[0].death;
+ 
+    $("#confirmed").append(total_confirmed);
+    $("recovered").append(total_recovered);
+    $("#deceased").append(total_deaths);
+    
+    
+ var myChart = document.getElementById('myChart').getContext('2d');
+ 
+ var chart = new Chart(myChart, {
+     type: 'bar',
+     fill: false,
+     data: {
+         labels: state,
+         datasets: [
+             {
+                 label: "Confirmed Cases",
+                 data: positive,
+                 backgroundColor: "#f1c40f",
+                 minBarLength: 100
+             },
+             {
+                 label: "Recovered Cases",
+                 data: recovered,
+                 backgroundColor: "green",
+                 minBarLength: 100
+             },
+             {
+                label: "Deceased",
+                data: death,
+                backgroundColor: "red",
+                minBarLength: 200
+            }
+         ]
+     },
+     options:{
+        title: {
+            display: true,
+            text: 'US STATES'
+        }
+     }
+ })
+   
+   })
 })
