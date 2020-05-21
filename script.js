488ec368f8f32f4240d3b06273b7ecf49a52161e
Paschal Ihenacho <ihenachopaschal@yahoo.com>
2020-05-21 04:32:43 -0500

    US Stats

--- script.js
+++ script.js
@@ -1,125 +1,8 @@
-// $(document).ready(function() {
-
-// "https://coronavirus-tracker-api.herokuapp.com/v2/locations?source=jhu&country_code=AU&province=South%20Australia&county=Australia&timelines=true"
-// 
-//      if(countryName != '' || countryCode != '' || countryProvince != '') {
-// *******     Code goes here
-//    }else{
-//            $("#error").html('Field cannot be empty');
-// //     }
-// var baseURL =  + countryName;
-//var countryName  = '';
-
-// var chartURL = "https://thevirustracker.com/timeline/map-data.json"
-
-// var chartURL = "https://api.apify.com/v2/datasets/jaycEQiGMlb7aOmtI/items?format=json&clean=1"
-//var chartURL = "https://api.covid19india.org/data.json"
-
-// var stateURL = "https://covidtracking.com/api/states";
-// var usURL = "http://covidtracking.com/api/us";
-// var infoURL = "https://covidtracking.com/api/states/info";
-
-//  $.ajax({
-//      url: stateURL,
-//     method: "GET",
-
-//  }).then(function(data) {
-  
-//     console.log(data + "USA")
-   
-//   var total_active, total_recovered, total_deaths, total_confirmed;
-
-//   // 4 empty arrays for chart
-//    var state = []
-//    var confirmed = []
-//    var recovered = []
-//    var deaths = []
-
-//     $.each(data.statewise, function(id, obj){
-//         state.push(obj.state)
-//         confirmed.push(obj.confirmed)
-//         recovered.push(obj.recovered)
-//         deaths.push(obj.deaths)
-
-  
-//   })
-
-//     state.shift()
-//     confirmed.shift()
-//     recovered.shift()
-//     deaths.shift()
-
-//    // console.log(state)
-     
-
-//    total_active = data.statewise[0].active;
-//    total_confirmed = data.statewise[0].confirmed;
-//    total_recovered = data.statewise[0].recovered;
-//    total_deaths = data.statewise[0].deaths;
-
-//    $("#active").append(total_active);
-//    $("#confirmed").append(total_confirmed);
-//    $("recovered").append(total_recovered);
-//    $("#deceased").append(total_deaths);
-   
-   
-// var myChart = document.getElementById('myChart').getContext('2d');
-
-// var chart = new Chart(myChart, {
-//     type: 'bar',
-//     fill: false,
-//     data: {
-//         labels: state,
-//         datasets: [
-//             {
-//                 label: "Confirmed Cases",
-//                 data: confirmed,
-//                 backgroundColor: "#f1c40f",
-//                 minBarLength: 100
-//             },
-//             {
-//                 label: "Recovered Cases",
-//                 data: recovered,
-//                 backgroundColor: "green",
-//                 minBarLength: 100
-//             }
-//         ]
-//     },
-//     options:{}
-// })
-
-
-
-
-//   var settings = {
-//     "async": true,
-//     "crossDomain": true,
-//     "url": "https://covid-19-data.p.rapidapi.com/report/country/name?date-format=YYYY-MM-DD&format=json&date=2020-05-01&name=usa",
-//     "method": "GET",
-//     "headers": {
-//         "x-rapidapi-host": "covid-19-data.p.rapidapi.com",
-//         "x-rapidapi-key": "409edfd192msh76f5ae7cd381374p17ad34jsn668e30d41ee5"
-//     }
-// }
-//var  queryParams = $("#countryInput").val();
-// $.ajax(settings).done(function (response) {
-//     console.log(response[0].provinces[0]);
-// });
-
-
-//  })
-
-
+ $(document).ready(function() {
 
 function buildQueryURL() {
    // if(queryParams != '') {
     // queryURL is the url we'll use to query the API
-        
-   // ******IMPORTANT******
-   //var queryURL =  "https://api.covid19api.com/live/country/US"
- 
-
-
     var  queryParams = $("#countryInput").val();
 
     var queryURL = "https://coronavirus-tracker-api.herokuapp.com/v2/locations?source=jhu&timelines=true&country=";
@@ -134,23 +17,22 @@ function buildQueryURL() {
 //         }
   }
 
-
-
+// This function appends searched country to the countryCard
 function showCountry(data) {
 
-        var totalConfirmedCountry = data.latest.confirmed; 
+    var totalConfirmedCountry = data.latest.confirmed; 
 
-        var totalDeathsCountry = data.latest.deaths
+    var totalDeathsCountry = data.latest.deaths
     
-        var countryPopulation = data.locations[0].country_population;
+    var countryPopulation = data.locations[0].country_population;
 
-        // Convert date
-        let lastUpaded = new Date(data.locations[0].last_updated);
-        let dd = lastUpaded.getDate();
+    // Convert date
+    let lastUpaded = new Date(data.locations[0].last_updated);
+    let dd = lastUpaded.getDate();
 
-        let mm = lastUpaded.getMonth()+1; 
-        const yyyy = lastUpaded.getFullYear();
-        if(dd<10) 
+    let mm = lastUpaded.getMonth()+1; 
+    const yyyy = lastUpaded.getFullYear();
+    if(dd<10) 
         {
         dd=`0${dd}`;
         } 
@@ -275,7 +157,7 @@ $.ajax({
         $('#global-deaths').text (totalDeathsGlobal)
      })
 
-     $(document).ready(function(){
+  
 
   //  var TestqueryURL =  "https://api.covid19api.com/live/country/China"
     //https://api.covid19api.com/summary
@@ -285,29 +167,39 @@ $.ajax({
     url: TestqueryURL,
     method: "GET",
 
-}).then(function(data) {
-  console.log(data)
-//    var totalDeath = 0;
-//    for (i = 0; i < data.length; i++) {
-//        totalDeath += data[i].Deaths;
-//    }
-//    console.log(totalDeath);
-
-
 })
 
 var infoURL = "https://covidtracking.com/api/states/info";
+var countryURL = "http://covidtracking.com/api/us";
 var stateURL = "https://covidtracking.com/api/states";
 
+//============Begin Country Stats (United States)===================//
 $.ajax({
- url: infoURL,
- method: "GET",
-
-}).then(function(info) {
-console.log(info)
-
-
+    url: countryURL,
+    method: "GET",
+   
+   }).then(function(countryURL) {
+       
+   var total_TestedResults, total_hospitalized, total_confirmed, onVentilator, total_recovered, total_deaths, last_updated;
+ 
+   total_TestedResults = countryURL[0].totalTestResults;
+   total_hospitalized = countryURL[0].hospitalized;
+   total_confirmed = countryURL[0].positive;
+   onVentilator = countryURL[0].onVentilatorCurrently;
+   total_recovered = countryURL[0].recovered;
+   total_deaths = countryURL[0].death;
+   last_updated = countryURL[0].lastModified;
+ 
+    $("#total_TestedResults").append(total_TestedResults);
+    $("#total_hospitalized").append(total_hospitalized);
+    $("#total_confirmed").append(total_confirmed);
+    $("#onVentilator").append(onVentilator);
+    $("#total_recovered").append(total_recovered);
+    $("#total_deaths").append(total_deaths);
+    $("#last_updated").append(last_updated);
 })
+//============End Country Stats (United States)===================//
+
 
 $.ajax({
     url: stateURL,
@@ -331,6 +223,8 @@ $.ajax({
  
    
    })
+
+   
  
     //  state.shift()
     //  confirmed.shift()
@@ -380,6 +274,14 @@ $.ajax({
         title: {
             display: true,
             text: 'US STATES'
+        },
+        scales: {
+            xAxes: [{
+                stacked: true
+            }],
+            yAxes: [{
+                stacked: true
+            }]
         }
      }
  })
